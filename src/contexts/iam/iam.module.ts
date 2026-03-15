822af3a19e3247dc0daf1c0a4b76b469ecd9e322
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2026-03-15 23:18:16 +0700

    feat: login flow, config docker compose to use jwt secret key

--- src/contexts/iam/iam.module.ts
+++ src/contexts/iam/iam.module.ts
@@ -1,17 +1,29 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { JwtModule } from '@nestjs/jwt';
 import { UserRepository } from './users/domain/user.repository';
 import { PrismaUserRepository } from './users/infrastructure/repositories/prisma-user.repository';
 import { UserCommandHandlers } from '../iam/users/application/commands';
 import { UserEventHandlers } from '../iam/users/application/events/handlers';
+import { UserQueryHandlers } from '../iam/users/application/queries';
 import { UsersController } from './users/infrastructure/http/controllers/users.controller';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [
+    CqrsModule,
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '3600s' },
+      }),
+    }),
+  ],
   controllers: [UsersController],
   providers: [
     ...UserCommandHandlers,
     ...UserEventHandlers,
+    ...UserQueryHandlers,
     {
       provide: UserRepository,
       useClass: PrismaUserRepository,
