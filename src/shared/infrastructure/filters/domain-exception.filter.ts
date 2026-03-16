b620d26407385cd8d9565c0401f0b726a491cd46
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2026-03-16 11:31:03 +0700

    feat: refactor login handler

--- src/shared/infrastructure/filters/domain-exception.filter.ts
+++ src/shared/infrastructure/filters/domain-exception.filter.ts
@@ -1,17 +1,47 @@
-import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
+// src/shared/infrastructure/filters/domain-exception.filter.ts
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpStatus,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { IdentityAlreadyExistsException } from '../../../contexts/iam/users/domain/exceptions/identity-already-exists.exception';
+import { InvalidCredentialsException } from '../../../contexts/iam/users/domain/exceptions/invalid-credentials.exception';
 
-@Catch(IdentityAlreadyExistsException)
+@Catch(Error)
 export class DomainExceptionFilter implements ExceptionFilter {
-  catch(exception: IdentityAlreadyExistsException, host: ArgumentsHost) {
+  private readonly exceptionMap = new Map<
+    string,
+    { status: number; error: string }
+  >([
+    [
+      IdentityAlreadyExistsException.name,
+      { status: HttpStatus.CONFLICT, error: 'Conflict' },
+    ],
+    [
+      InvalidCredentialsException.name,
+      { status: HttpStatus.UNAUTHORIZED, error: 'Unauthorized' },
+    ],
+  ]);
+
+  catch(exception: Error, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    return response.status(409).json({
-      statusCode: 409,
-      message: exception.message,
-      error: 'Conflict',
+    const exceptionConfig = this.exceptionMap.get(exception.constructor.name);
+    if (exceptionConfig) {
+      return response.status(exceptionConfig.status).json({
+        statusCode: exceptionConfig.status,
+        message: exception.message,
+        error: exceptionConfig.error,
+      });
+    }
+
+    return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      statusCode: 500,
+      message: 'An unexpected error occurred in this reality',
+      error: 'Internal Server Error',
     });
   }
 }
